bb8bfc1b22095d4e3957c472928622293760f2ac
bahrom2101 <jabborov0121@gmail.com>
2023-04-03 14:18:58 +0500

    first bad version

---
+++ lib/math/278_first_bad_version.dart
@@ -0,0 +1,21 @@
+void main() {
+  print((1 / 2).floor());
+  print(firstBadVersion(2));
+}
+
+int firstBadVersion(int n) {
+  int left = 1, right = n;
+  while (left < right) {
+    int mid = left + (right - left) ~/ 2;
+    if (isBadVersion(mid)) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+}
+
+bool isBadVersion(int version) {
+  return version >= 1;
+}
