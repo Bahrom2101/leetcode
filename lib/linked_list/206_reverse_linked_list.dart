3ff4ae950df9ebf4ec780dd748bf87f9c57d0953
bahrom2101 <jabborov0121@gmail.com>
2023-04-03 11:31:15 +0500

    reverse

--- lib/linked_list/206_reverse_linked_list.dart
+++ lib/linked_list/206_reverse_linked_list.dart
@@ -2,17 +2,58 @@ import 'package:leetcode/structures/list_node.dart';
 
 void main(List<String> args) {
   ListNode list = ListNode(2);
-  list.next = ListNode(1);
-  list.next?.next = ListNode(3);
-  list.next?.next?.next = ListNode(5);
-  list.next?.next?.next?.next = ListNode(6);
-  list.next?.next?.next?.next?.next = ListNode(4);
-  list.next?.next?.next?.next?.next?.next = ListNode(7);
-  print(reverseList(list));
+  ListNode? l = list;
+  for (int i = 0; i < 200000000; i++) {
+    l?.next = ListNode(i);
+    l = l?.next;
+  }
+  var a = DateTime.now();
+  reverseList1(list.next);
+  var b = DateTime.now();
+  print(b.difference(a).inMilliseconds);
+  a = DateTime.now();
+  reverseList2(list.next);
+  b = DateTime.now();
+  print(b.difference(a).inMilliseconds);
+}
+// 7533
+// 1013
+
+ListNode? reverseList1(ListNode? head) {
+  if (head == null) {
+    return null;
+  } else if (head.next == null) {
+    return head;
+  } else {
+    List<int> stack = [];
+
+    while (head != null) {
+      stack.add(head.val);
+      head = head.next;
+    }
+
+    for (var i = 0; i < stack.length; i++) {
+      var temp = ListNode(stack[i]);
+      temp.next = head;
+      head = temp;
+    }
+    return head;
+  }
+
+  // return reverseRecursively(null, head);
 }
 
-ListNode? reverseList(ListNode? head) {
-  return reverseRecursively(null, head);
+ListNode? reverseList2(ListNode? head) {
+  ListNode? current = head;
+  ListNode? prev;
+
+  while (current != null) {
+    ListNode? next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
 }
 
 ListNode? reverseRecursively(ListNode? prev, ListNode? head) {
