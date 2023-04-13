282a306cc24a85645a81636afe7ccae39e7cc21f
bahrom2101 <jabborov0121@gmail.com>
2023-04-13 10:47:14 +0500

    character replacement (not done)

--- lib/string/424_character_replacement.dart
+++ lib/string/424_character_replacement.dart
@@ -1,28 +1,53 @@
 void main() {
-  print(characterReplacement('TADSTRATBBFTT', 2));
+  print(characterReplacement('BAAAB', 2));
 }
 
+//18119565334174252
+
 int characterReplacement(String s, int k) {
-  if(s.length == k) return k;
-  var sFreq = List.filled(26, 0);
-  Map<String, List<int>> map = {};
+  if (s.length == k) return k;
+  List<int> commons1 = List.filled(s.length, 1);
+  List<int> commons2 = List.filled(s.length, 1);
+  int stepped = 0;
   for (int i = 0; i < s.length; i++) {
-    if (map[s[i]] != null) {
-      map[s[i]]!.add(i);
-    } else {
-      map[s[i]] = [i];
-    }
-  }
-  print(map);
-  int maxCommon = 0;
-  for (var list in map.values) {
-    for(int i = 1; i < list.length; i++) {
-      var distance = list[i] - list[i - 1] - 1;
-      if(distance == k) {
-        maxCommon = distance + 2;
-      } else if(distance < k) {
-        maxCommon = distance + 2;
+    String pointer = s[i];
+    for (int j = i + 1; j < s.length; j++) {
+      if (s[j] == pointer && stepped != k) {
+        commons1[i]++;
+      } else if (stepped != k) {
+        commons1[i]++;
+        stepped++;
+      } else if (s[j] == pointer) {
+        commons1[i]++;
+      } else {
+        break;
       }
+    }
+    stepped = 0;
+  }
+  for (int i = s.length - 1; i >= 0; i--) {
+    String pointer = s[i];
+    for (int j = i - 1; j >= 0; j--) {
+      if (s[j] == pointer && stepped != k) {
+        commons2[i]++;
+      } else if (stepped != k) {
+        commons2[i]++;
+        stepped++;
+      } else if (s[j] == pointer) {
+        commons2[i]++;
+      } else {
+        break;
+      }
+    }
+    stepped = 0;
+  }
+  int maxCommon = 0;
+  for (int i = 0; i < commons1.length; i++) {
+    if (commons1[i] > maxCommon) {
+      maxCommon = commons1[i];
+    }
+    if (commons2[i] > maxCommon) {
+      maxCommon = commons2[i];
     }
   }
   return maxCommon;
