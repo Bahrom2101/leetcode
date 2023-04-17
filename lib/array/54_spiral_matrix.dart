7ab4b8af44d043e2ee5d3482fc78bf459d2072c3
bahrom2101 <jabborov0121@gmail.com>
2023-04-17 18:14:03 +0500

    spiral matrix

--- lib/array/54_spiral_matrix.dart
+++ lib/array/54_spiral_matrix.dart
@@ -1,7 +1,50 @@
 void main() {
-
+  print(spiralOrder([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+  ]));
 }
 
-List<int> spiral(List<List<int>> matrix) {
-  return [];
+List<int> spiralOrder(List<List<int>> matrix) {
+  if (matrix.isEmpty) return [];
+
+  int top = 0;
+  int bottom = matrix.length - 1;
+  int left = 0;
+  int right = matrix[0].length - 1;
+  List<int> spiralList = [];
+
+  while (top <= bottom && left <= right) {
+    // Traverse Right
+    for (int i = left; i <= right; i++) {
+      spiralList.add(matrix[top][i]);
+    }
+    top++;
+
+    // Traverse Down
+    for (int i = top; i <= bottom; i++) {
+      spiralList.add(matrix[i][right]);
+    }
+    right--;
+
+    if (top <= bottom) {
+      // Traverse Left
+      for (int i = right; i >= left; i--) {
+        spiralList.add(matrix[bottom][i]);
+      }
+      bottom--;
+    }
+
+    if (left <= right) {
+      // Traverse Up
+      for (int i = bottom; i >= top; i--) {
+        spiralList.add(matrix[i][left]);
+      }
+      left++;
+    }
+  }
+
+  return spiralList;
 }
