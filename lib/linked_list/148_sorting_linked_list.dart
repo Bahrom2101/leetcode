ef4f8c0e21c127c546ff12bf157cbcd4c9c39098
bahrom2101 <jabborov0121@gmail.com>
2023-04-26 15:56:09 +0500

    sorting linked list O(n log n)

--- lib/linked_list/148_sorting_linked_list.dart
+++ lib/linked_list/148_sorting_linked_list.dart
@@ -3,18 +3,59 @@
 import '../structures/list_node.dart';
 
 void main(List<String> args) {
-  ListNode list = ListNode(5);
-  list.next = ListNode(23);
-  list.next?.next = ListNode(2);
-  list.next?.next?.next = ListNode(13);
-  list.next?.next?.next?.next = ListNode(100);
-  list.next?.next?.next?.next?.next = ListNode(10);
+  ListNode list = ListNode(-1);
+  list.next = ListNode(5);
+  list.next?.next = ListNode(3);
+  list.next?.next?.next = ListNode(6);
+  list.next?.next?.next?.next = ListNode(0);
   print(sortList(list));
 }
 
 ListNode? sortList(ListNode? head) {
-  while (head?.next != null) {
-    if ((head?.val ?? 0) > (head?.next?.val ?? 0)) {}
+  return mergeSort(head);
+}
+
+ListNode? mergeSort(ListNode? list) {
+  int length = 0;
+  var temp = list;
+  while (temp != null) {
+    temp = temp.next;
+    length++;
   }
-  return null;
+  if (length < 2) return list;
+  final middle = length ~/ 2;
+
+  ListNode? newList = ListNode();
+  temp = newList;
+  while (length != middle) {
+    temp?.next = ListNode(list!.val);
+    temp = temp?.next;
+    list = list?.next;
+    length--;
+  }
+  final left = mergeSort(newList.next);
+  final right = mergeSort(list);
+  return mergeTwoLists(left, right);
+}
+
+ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+  ListNode? newList = ListNode();
+  ListNode? temp = newList;
+  while (list1 != null && list2 != null) {
+    if (list1.val < list2.val) {
+      temp?.next = list1;
+      list1 = list1.next;
+    } else {
+      temp?.next = list2;
+      list2 = list2.next;
+    }
+    temp = temp?.next;
+  }
+  if (list1 != null) {
+    temp?.next = list1;
+  }
+  if (list2 != null) {
+    temp?.next = list2;
+  }
+  return newList.next;
 }
