b1f2f0417f1f4d1ee3bbf51fad6ea0d047a4899d
bahrom2101 <jabborov0121@gmail.com>
2023-04-27 09:56:59 +0500

    making longest palindrome from list of words

---
+++ lib/hashtable/2131_longest_palindrome.dart
@@ -0,0 +1,34 @@
+void main() {
+  // print(longestPalindrome(["lc", "cl", "gg"]));
+  print(longestPalindrome(["ab", "ty", "yt", "lc", "cl", "ab"]));
+  // print(longestPalindrome(["cc", "ll", "xx"]));
+}
+
+int longestPalindrome(List<String> words) {
+  Map<String, int> map = {};
+  for (int i = 0; i < words.length; i++) {
+    if (map[words[i]] == null) {
+      map[words[i]] = 1;
+    } else {
+      map[words[i]] = map[words[i]]! + 1;
+    }
+  }
+  print(map);
+  String equals = '';
+  String left = '';
+  String right = '';
+  var list = [];
+  for (int i = 0; i < words.length; i++) {
+    var reversed = reverse(words[i]);
+    if (map[words[i]] != null || map[reversed] != null) {
+      list.add(words[i]);
+    }
+  }
+  print(list);
+  int count = 0;
+  return count;
+}
+
+String reverse(String word) {
+  return '${word[1]}${word[0]}';
+}
