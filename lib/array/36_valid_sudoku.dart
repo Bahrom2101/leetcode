6d906f5882ee025734b1d9c5df9f385b8e74c64a
bahromjabborov <jabborov0121@gmail.com>
2026-05-03 10:05:31 +0500

    36. valid sudoku done

--- lib/array/36_valid_sudoku.dart
+++ lib/array/36_valid_sudoku.dart
@@ -1,6 +1,6 @@
 void main() {
   print(isValidSudoku([
-    ["5", "8", ".", ".", "7", ".", ".", ".", "."],
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
     [".", "9", "8", ".", ".", ".", ".", "6", "."],
     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
@@ -32,6 +32,24 @@ bool isValidSudoku(List<List<String>> board) {
       }
     }
     if (vectorNumbers != vector.length) return false;
+  }
+  List<Set<String>> blocks = []; // 3x3 squares
+  for (int r = 0; r < board.length; r += 3) {
+    for (int c = 0; c < board[r].length; c += 3) {
+      Set<String> block = {};
+
+      int vectorNumbers = 0;
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+          if (board[r + i][c + j] != '.') {
+            vectorNumbers++;
+            block.add(board[r + i][c + j]);
+          }
+        }
+      }
+      if (vectorNumbers != block.length) return false;
+      blocks.add(block);
+    }
   }
   return true;
 }
