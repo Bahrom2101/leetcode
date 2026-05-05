1854769e2218be7b5a37f93dd7160d8995c05473
bahromjabborov <jabborov0121@gmail.com>
2026-05-05 18:38:29 +0500

    37. sudoku solver (not done)

--- lib/array/37_sudoku_solver.dart
+++ lib/array/37_sudoku_solver.dart
@@ -1,5 +1,5 @@
 void main() {
-  var sudoku = [
+  var board = [
     /*        0    1    2    3    4    5    6    7    8      */
     /* 0 */ ["5", "3", ".", ".", "7", ".", ".", ".", "."],
     /* 1 */ ["6", ".", ".", "1", "9", "5", ".", ".", "."],
@@ -11,89 +11,107 @@ void main() {
     /* 7 */ [".", ".", ".", "4", "1", "9", ".", ".", "5"],
     /* 8 */ [".", ".", ".", ".", "8", ".", ".", "7", "9"]
   ];
-  solveSudoku(sudoku);
+  Solution().solveSudoku(board);
+  for (var item in board) {
+    print(item);
+  }
 }
 
-void solveSudoku(List<List<String>> board) {
-  List<Set<String>> rows = [];
-  List<Set<String>> columns = [];
-  for (int i = 0; i < board.length; i++) {
-    Set<String> row = {};
-    Set<String> column = {};
-    for (int j = 0; j < board[i].length; j++) {
-      if (board[i][j] != '.') {
-        row.add(board[i][j]);
+class Solution {
+  void solveSudoku(List<List<String>> board) {
+    List<Set<String>> rows = [];
+    List<Set<String>> columns = [];
+    List<Set<String>> blocks = []; // 3x3 squares
+
+    for (int i = 0; i < board.length; i++) {
+      Set<String> row = {};
+      Set<String> column = {};
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != '.') {
+          row.add(board[i][j]);
+        }
+        if (board[j][i] != '.') {
+          column.add(board[j][i]);
+        }
       }
-      if (board[j][i] != '.') {
-        column.add(board[j][i]);
+      rows.add(row);
+      columns.add(column);
+    }
+    for (int r = 0; r < board.length; r += 3) {
+      for (int c = 0; c < board[r].length; c += 3) {
+        Set<String> block = {};
+        for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 3; j++) {
+            if (board[r + i][c + j] != '.') {
+              block.add(board[r + i][c + j]);
+            }
+          }
+        }
+        blocks.add(block);
       }
     }
-    rows.add(row);
-    columns.add(column);
+    changeCells(board, rows, columns, blocks);
   }
-  List<Set<String>> blocks = []; // 3x3 squares
-  for (int r = 0; r < board.length; r += 3) {
-    for (int c = 0; c < board[r].length; c += 3) {
-      Set<String> block = {};
-      for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-          if (board[r + i][c + j] != '.') {
-            block.add(board[r + i][c + j]);
+
+  void changeCells(
+    List<List<String>> board,
+    List<Set<String>> rows,
+    List<Set<String>> columns,
+    List<Set<String>> blocks,
+  ) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == '.') {
+          var blockNumber = defineBlock(i, j);
+          var row = rows[i];
+          var col = columns[j];
+          var block = blocks[blockNumber - 1];
+          print('row: $row');
+          print('col: $col');
+          print('block: $block');
+          for (int d = 1; d <= 9; d++) {
+            print('d: $d');
+            print('canReplace: ${canReplace(d, row, col, block)}');
+            if (canReplace(d, row, col, block)) {
+              board[i][j] = d.toString();
+              row.add(d.toString());
+              col.add(d.toString());
+              block.add(d.toString());
+              break;
+            } else {
+              print('else d: $d');
+            }
           }
         }
       }
-      blocks.add(block);
     }
   }
 
-  for (int i = 0; i < board.length; i++) {
-    for (int j = 0; j < board[i].length; j++) {
-      if (board[i][j] == '.') {
-        print('point: $i:$j');
-        var blockNumber = defineBlock(i, j);
-        var row = rows[i];
-        var column = columns[j];
-        var block = blocks[blockNumber - 1];
-        print('block number: $blockNumber');
-        print('row: $row, colum: $column, block: $block');
-        // if ({...row, ...column}.length == 9) {}
-        var random = randomDigit(row, column, block);
-        // print('random number: $random');
-        board[i][j] = random;
-        row.add(random);
-        column.add(random);
-        block.add(random);
-      }
-    }
-    print('list: ${board[i]}');
-  }
-}
-
-int defineBlock(int i, int j) {
-  if (i < 3) {
-    if (j < 3) return 1;
-    if (j >=3 && j < 6 ) return 2;
-    if (j >=6 && j < 9 ) return 3;
-  } if (i >= 3 && i < 6) {
-    if (j < 3) return 4;
-    if (j >=3 && j < 6 ) return 5;
-    if (j >=6 && j < 9 ) return 6;
-  } else if (i >= 6 && i < 9) {
-    if (j < 3) return 7;
-    if (j >=3 && j < 6 ) return 8;
-    if (j >=6 && j < 9 ) return 9;
-  }
-  return 0;
-}
-
-String randomDigit(Set<String> a, Set<String> b, Set<String> c) {
-  // print('a: $a');
-  // print('b: $b');
-  // print('c: $c');
-  for (int i = 0; i < 10; i++) {
-    if (!a.contains('$i') && !b.contains('$i') && !c.contains('$i')) {
-      return '$i';
+  bool canReplace(int d, Set<String> row, Set<String> col, Set<String> block) {
+    if (row.contains(d.toString()) ||
+        col.contains(d.toString()) ||
+        block.contains(d.toString())) {
+      return false;
+    } else {
+      return true;
     }
   }
-  return randomDigit(a, b, c);
+
+  int defineBlock(int i, int j) {
+    if (i < 3) {
+      if (j < 3) return 1;
+      if (j >= 3 && j < 6) return 2;
+      if (j >= 6 && j < 9) return 3;
+    }
+    if (i >= 3 && i < 6) {
+      if (j < 3) return 4;
+      if (j >= 3 && j < 6) return 5;
+      if (j >= 6 && j < 9) return 6;
+    } else if (i >= 6 && i < 9) {
+      if (j < 3) return 7;
+      if (j >= 3 && j < 6) return 8;
+      if (j >= 6 && j < 9) return 9;
+    }
+    return 0;
+  }
 }
