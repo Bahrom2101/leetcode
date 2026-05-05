a7da8a0fa11667db5a3be2860a6c98246aa178f9
bahromjabborov <jabborov0121@gmail.com>
2026-05-05 23:55:15 +0500

    37. sudoku solver (not done)

--- lib/array/37_sudoku_solver.dart
+++ lib/array/37_sudoku_solver.dart
@@ -12,79 +12,36 @@ void main() {
     /* 8 */ [".", ".", ".", ".", "8", ".", ".", "7", "9"]
   ];
   Solution().solveSudoku(board);
-  for (var item in board) {
-    print(item);
-  }
 }
 
 class Solution {
-  void solveSudoku(List<List<String>> board) {
-    List<Set<String>> rows = [];
-    List<Set<String>> columns = [];
-    List<Set<String>> blocks = []; // 3x3 squares
+  late List<List<String>> board;
+  List<Set<String>> rows = List.generate(9, (e) => <String>{});
+  List<Set<String>> columns = List.generate(9, (e) => <String>{});
+  List<Set<String>> boxes = List.generate(9, (e) => <String>{}); // 3x3 squares
 
-    for (int i = 0; i < board.length; i++) {
-      Set<String> row = {};
-      Set<String> column = {};
-      for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] != '.') {
-          row.add(board[i][j]);
-        }
-        if (board[j][i] != '.') {
-          column.add(board[j][i]);
-        }
-      }
-      rows.add(row);
-      columns.add(column);
-    }
-    for (int r = 0; r < board.length; r += 3) {
-      for (int c = 0; c < board[r].length; c += 3) {
-        Set<String> block = {};
-        for (int i = 0; i < 3; i++) {
-          for (int j = 0; j < 3; j++) {
-            if (board[r + i][c + j] != '.') {
-              block.add(board[r + i][c + j]);
-            }
-          }
-        }
-        blocks.add(block);
-      }
-    }
-    changeCells(board, rows, columns, blocks);
-  }
+  bool changeCells(int r) {
+    int i = r ~/ 9;
+    int j = r % 9;
+    if (i == 81) return true;
+    if (board[i][j] != '.') return changeCells(r + 1);
 
-  void changeCells(
-    List<List<String>> board,
-    List<Set<String>> rows,
-    List<Set<String>> columns,
-    List<Set<String>> blocks,
-  ) {
-    for (int i = 0; i < board.length; i++) {
-      for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j] == '.') {
-          var blockNumber = defineBlock(i, j);
-          var row = rows[i];
-          var col = columns[j];
-          var block = blocks[blockNumber - 1];
-          print('row: $row');
-          print('col: $col');
-          print('block: $block');
-          for (int d = 1; d <= 9; d++) {
-            print('d: $d');
-            print('canReplace: ${canReplace(d, row, col, block)}');
-            if (canReplace(d, row, col, block)) {
-              board[i][j] = d.toString();
-              row.add(d.toString());
-              col.add(d.toString());
-              block.add(d.toString());
-              break;
-            } else {
-              print('else d: $d');
-            }
-          }
-        }
-      }
+    var blockNumber = i ~/ 3 * 3 + j ~/ 3;
+    for (int d = 1; d <= 9; d++) {
+      if (!canReplace(d, rows[i], columns[j], boxes[blockNumber])) continue;
+      board[i][j] = d.toString();
+      rows[i].add(d.toString());
+      columns[j].add(d.toString());
+      boxes[blockNumber].add(d.toString());
+
+      if (changeCells(r + 1)) return true;
+
+      board[i][j] = '.';
+      rows[i].remove(d);
+      columns[j].remove(d);
+      boxes[blockNumber].remove(d);
     }
+    return false;
   }
 
   bool canReplace(int d, Set<String> row, Set<String> col, Set<String> block) {
@@ -97,21 +54,23 @@ class Solution {
     }
   }
 
-  int defineBlock(int i, int j) {
-    if (i < 3) {
-      if (j < 3) return 1;
-      if (j >= 3 && j < 6) return 2;
-      if (j >= 6 && j < 9) return 3;
+  void solveSudoku(List<List<String>> board) {
+    this.board = board;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] != '.') {
+          rows[i].add(board[i][j]);
+          boxes[i ~/ 3 * 3 + j ~/ 3].add(board[i][j]);
+        }
+        if (board[j][i] != '.') {
+          columns[i].add(board[j][i]);
+        }
+      }
     }
-    if (i >= 3 && i < 6) {
-      if (j < 3) return 4;
-      if (j >= 3 && j < 6) return 5;
-      if (j >= 6 && j < 9) return 6;
-    } else if (i >= 6 && i < 9) {
-      if (j < 3) return 7;
-      if (j >= 3 && j < 6) return 8;
-      if (j >= 6 && j < 9) return 9;
+
+    changeCells(0);
+    for (var e in board) {
+      print(e);
     }
-    return 0;
   }
 }
