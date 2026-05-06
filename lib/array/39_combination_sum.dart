b070c6dadbd9ad35b198083249b4b2ebd68c47b0
bahromjabborov <jabborov0121@gmail.com>
2026-05-06 23:22:16 +0500

    39. combination sum fixes

--- lib/array/39_combination_sum.dart
+++ lib/array/39_combination_sum.dart
@@ -5,8 +5,8 @@ void main() {
     [2, 2, 2, 2, 3, 3, 3, 3], // 20 = 2 * 4 + 3 * 4
     [2, 3, 3, 3, 3, 3, 3] // 20 = 2 * 1 + 3 * 6
   ];
-  print(Solution().combinationSum([8, 3,7, 4], 11));
-  // print(Solution().combinationSum([2, 3], 8));
+  print(Solution().combinationSum([8, 3, 7, 4], 11));
+  // print(Solution().combinationSum([2, 3, 5], 10));
   // print(Solution().combinationSum([2, 3, 6, 7], 7)); // [[2,2,3],[7]]
   // print(Solution().combinationSum([2, 3, 5], 8)); // [[2,2,2,2],[2,3,3],[3,5]]
 }
@@ -16,28 +16,24 @@ class Solution {
     Set<String> combinations = {};
     for (int i = 0; i < candidates.length; i++) {
       int varI = candidates[i];
-      print('varI=[$i]: $varI');
       for (int j = 0; j <= i; j++) {
         int varJ = candidates[j];
-        print('varJ=[$j]: $varJ');
         int countDown = target;
         while (countDown > 0) {
           var sub = countDown - varJ;
-          if (sub % varI == 0) {
+          if (sub >= 0 && sub % varI == 0) {
             List<String> list = [
               ...List.generate(sub ~/ varI, (_) => varI.toString()),
               ...List.generate((target - sub) ~/ varJ, (_) => varJ.toString())
             ];
-            combinations.add(list.join(''));
-            print('list: $list');
+            combinations.add(list.join(','));
           }
           countDown -= varJ;
         }
       }
-      print('-' * 100);
     }
     return combinations
-        .map((e) => List.generate(e.length, (g) => int.parse(e[g])))
+        .map((e) => e.split(',').map(int.parse).toList())
         .toList();
   }
 }
