7e5ed24d66cfb0c9c5c6287002a3d7efdb13b4d5
bahromjabborov <jabborov0121@gmail.com>
2026-05-17 01:29:50 +0500

    40. combination sum 2 (not done)

--- bin/leetcode.dart
+++ bin/leetcode.dart
@@ -1,1 +1,53 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  subsets([4, 5, 3, 6], 1, []);
+  // [5,3,6] [5,3] [5,6] [5] [3,6] [3] [6] []
+}
+
+void subsets(
+  List<int> nums,
+  int index,
+  List<int> current,
+) {
+  if (nums.isEmpty) {
+    print(current);
+    return;
+  }
+  var sublist = nums.sublist(index);
+  current.addAll(sublist);
+  subsets(sublist, index + 1, current);
+}
+
+void binaryStrings(String current, int n) {
+  if (n == 0) {
+    print(current);
+    return;
+  }
+  binaryStrings('${current}0', n - 1);
+  binaryStrings('${current}1', n - 1);
+}
+
+int fibonacci(int n, [int start = 0, int end = 1]) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+String reverse(String n) {
+  if (n.length == 1) return n[0];
+  return n[n.length - 1] + reverse(n.substring(0, n.length - 1));
+}
+
+int power(int base, int exp) {
+  if (exp == 1) return base;
+
+  return base * power(base, exp - 1);
+}
+
+int sum(int n) {
+  if (n == 0) return 0;
+  return n + sum(n - 1);
+}
+
+int factorial(int n) {
+  if (n == 0) return 1;
+  return n * factorial(n - 1);
+}
--- lib/array/40_combination_sum_2.dart
+++ lib/array/40_combination_sum_2.dart
@@ -1,5 +1,7 @@
 void main() {
-  print(Solution().combinationSum2([10,1,2,7,6,1,5], 8)); //[1,1,6], [1,2,5], [1,7], [2,6]
+  // print(Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)); //[1,1,6], [1,2,5], [1,7], [2,6]
+  print(Solution().combinationSum2([2, 5, 2, 1, 2], 5)); //[1,2,2], [5]
+  print(Solution().combinationSum2([2, 3], 5)); //[1,2,2], [5]
 }
 
 class Solution {
@@ -7,6 +9,9 @@ class Solution {
     Set<String> combinations = {};
 
     void build(int start, int remaining, List<int> current) {
+      print('start: $start');
+      print('remaining: $remaining');
+      print('current: $current');
       if (remaining == 0) {
         combinations.add(current.join(','));
         return;
@@ -24,6 +29,7 @@ class Solution {
     }
 
     build(0, target, []);
+    print('combinations:$combinations');
 
     return combinations
         .map((e) => e.split(',').map(int.parse).toList())
