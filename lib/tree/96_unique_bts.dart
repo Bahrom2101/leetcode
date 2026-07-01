6ae18ac491193347efa50c7fc6c8793296272804
bahromjabborov <jabborov0121@gmail.com>
2026-07-01 16:07:27 +0500

    96. unique binary search trees (not done)

---
+++ lib/tree/96_unique_bts.dart
@@ -0,0 +1,16 @@
+import 'package:leetcode/structures/tree_node.dart';
+
+void main() {
+  print(Solution().numTrees(3));
+}
+
+class Solution {
+  int numTrees(int n) {
+    List<TreeNode?> trees = [];
+    for (int i = 1; i <= n; i++) {
+      
+    }
+    print(trees[0]?.left);
+    return 0;
+  }
+}
