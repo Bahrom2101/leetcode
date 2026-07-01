b12adb7652f444ee62d74024337d6bdf7b7a7c9b
bahromjabborov <jabborov0121@gmail.com>
2026-07-01 15:21:59 +0500

    42. trapping rain water (not done yet)

---
+++ lib/array/42_trapping_rain_water.dart
@@ -0,0 +1,13 @@
+void main() {
+  print(Solution().trap([4, 2, 0, 3, 2, 5]));
+  // print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+}
+
+class Solution {
+  int trap(List<int> height) {
+    if (height.length <= 1) return 0;
+    int max = 0;
+    
+    return max;
+  }
+}
